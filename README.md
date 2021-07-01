5178a77ff45f34d0b7cf09eb40015a1e72619422
Hunter Chasens <hchasens@Hunters-MacBook-Air.local>
2021-07-01 15:58:16 -0400

    fleshed out the MVP

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# xorCrypt
+xorCrypt is a simple program that acts on two files of the same size and xors the second with the first. This allows for simple encryption. If only one file is provided then a new file is created, of the same length of passed file.
+
 xorCrypt takes two files as an input. The first file is the key, the second is the data file. The key and the datafile are the same size. A key is generated for a datafile if no key is given (one argument is passed).
 
 Insperation from Project Euler 
--- xorCrypt.c
+++ xorCrypt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
-int keyGen(FILE *data); // if only one file is passed, it generates a random key of length `file` named "`file`.xorKey"
-int crypt(FILE *key, FILE *data); // if two files are passsed decrypt/encrypt the second file by xoring it with the first.
+FILE * keyGen(FILE *data); // if only one file is passed, it generates a random key of length `file` named "`file`.xorKey"
+FILE * crypt(FILE *key, FILE *data); // if two files are passsed decrypt/encrypt the second file by xoring it with the first.
 void help(); // triggered if sytax isn't right.
 
 
@@ -14,13 +15,30 @@ void help(); // triggered if sytax isn't right.
 
 
 int main(int argc, char *argv[]){
-	FILE *data;
-	FILE *key;
+	FILE *first; 		// first argument passed
+	FILE *second;		// second file passed
+	FILE *output;
 
-	if(argc <= 1) help(); 	//if there's 1 argument or less run help (if no arguments are passed)
-	if(argc == 2)  keyGen(data); //if there are 2 arguments or less generate a key for the given data file (if one argument is passed)
-	if(argc == 3)  crypt(key, data); 	//if there are 3 arguemtns (two files passed) xore the second with the first
-	if(argc > 3) help();	//if there are 4 or more argumetns (more then two files passed) run help
+	// open first file passed
+	first = fopen(argv[2], "r");	
+	// open second file passed
+	second = fopen(argv[3], "r");	
+	if(argc <= 1 || argc > 3) help(); 		//if there's 1 argument or less run help (if no arguments are passed) or if there are 4 or more argumetns (more then two files passed) run help
+
+
+	if(argc == 2){
+		
+		// open/create file output
+		output = fopen(strcat(argv[2], ".key"), "w");
+		//fwrite(keyGen(first), ; 		//if there are 2 arguments or less generate a key for the given data file (if one argument is passed)
+	}
+
+	if(argc == 3){
+		// open/create file output
+		output = fopen(strcat(argv[3], ".out"), "w");
+		output = crypt(first, second); 	//if there are 3 arguemtns (two files passed) xore the second with the first
+	}	
+
 
 	return 0;
 }
@@ -29,12 +47,12 @@ void help(){ //prints help
 	printf("help text"); // @TODO
 }
 
-int keyGen(FILE *data){ //prints help
+FILE * keyGen(FILE *data){ //prints help
 	printf("Key gen"); // @TODO
-	return 0;
+	return data;
 }
 
-int crypt(FILE *key, FILE *data){ //prints help
+FILE * crypt(FILE *key, FILE *data){ //prints help
 	printf("crypt"); // @TODO
-	return 0;
+	return key;
 }
